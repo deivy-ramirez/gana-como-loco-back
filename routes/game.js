eedc00ad72c84b49f6ab5ad8d946c10055f1d0b0
deivy-ramirez <davydwindsor@gmail.com>
2024-10-30 11:43:37 -0500

    modificacion de la ruta game

--- routes/game.js
+++ routes/game.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const Code = require('../models/Code');
 const generateCodes = require('../utils/codeGenerator');
+const verifyCode = require('../controllers/codeController');
 const router = express.Router();
 
-router.post('/generate-codes', async (req, res) => {
+/*router.post('/generate-codes', async (req, res) => {
   try {
     const result = await generateCodes();
     res.json(result);
@@ -11,6 +12,6 @@ router.post('/generate-codes', async (req, res) => {
     console.error('Error al generar códigos:', error);
     res.status(500).json({ message: 'Error al generar códigos' });
   }
-});
+});*/
 
 module.exports = router;
