9106502c31aa999313c9a054d1b718ebb4f87853
deivy-ramirez <davydwindsor@gmail.com>
2024-10-29 23:59:56 -0500

    instalacion del bcryptjs

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 const Auth = require('../models/Auth');
 const router = express.Router();
