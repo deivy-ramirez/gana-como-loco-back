63e43f8d12d96ab88c32a5d3226ca9d88e10e1e8
deivy-ramirez <davydwindsor@gmail.com>
2024-11-01 20:51:42 -0500

    adicion del admin panel20.0

--- models/CodeUsage.js
+++ models/CodeUsage.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const codeUsageSchema = new mongoose.Schema({
   codeId: { type: mongoose.Schema.Types.ObjectId, ref: 'Code', required: true },
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User ', required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   fechaUso: { type: Date, default: Date.now }
 });
 
