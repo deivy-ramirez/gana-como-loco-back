e0341d48cb622c93ff96149909a9a84bf71af830
deivy-ramirez <davydwindsor@gmail.com>
2024-11-01 20:41:26 -0500

    adicion del admin panel19.0

--- controllers/codeController.js
+++ controllers/codeController.js
@@ -6,9 +6,9 @@ exports.verifyCode = async (req, res, next) => {
     const code = await Code.findOne({ codigo, usado: false });
     if (code) {
       code.usado = true;
-      code.usadoPor = userId;
       code.fechaUso = new Date();
       await code.save();
+      await CodeUsage.create({ codeId: code._id, userId });
       res.json({ message: 'Código válido', premio: code.premio });
     } else {
       res.status(400).json({ message: 'Código inválido o ya usado' });
