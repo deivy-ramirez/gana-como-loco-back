29d4f13c4295de3c8cabf4ddb1421bdac9f99608
deivy-ramirez <davydwindsor@gmail.com>
2024-11-01 19:49:20 -0500

    adicion del admin panel16.0

--- controllers/codeController.js
+++ controllers/codeController.js
@@ -1,34 +1,41 @@
-// controllers/codeController.js
+const Code = require('../models/Code');
 const CodeUsage = require('../models/CodeUsage');
 
 exports.verifyCode = async (req, res, next) => {
   try {
     const { codigo, userId } = req.body;
     
-    // Verificar que el userId existe
-    if (!userId) {
-      return res.status(400).json({ message: 'Usuario no especificado' });
-    }
+    // Agregamos logs para debuggear
+    console.log('Datos recibidos:', { codigo, userId });
 
+    // Verificar el código primero
     const code = await Code.findOne({ codigo, usado: false });
     
-    if (code) {
-      // Crear un nuevo registro en CodeUsage
-      const newCodeUsage = new CodeUsage({
-        codeId: code._id,
-        userId: userId,
-        fechaUso: new Date()
-      });
-      await newCodeUsage.save();
-
-      // Marcar el código como usado
-      code.usado = true;
-      await code.save();
-      
-      res.json({ message: 'Código válido', premio: code.premio });
-    } else {
-      res.status(400).json({ message: 'Código inválido o ya usado' });
+    if (!code) {
+      return res.status(400).json({ message: 'Código inválido o ya usado' });
     }
+
+    if (!userId) {
+      return res.status(400).json({ message: 'Debes iniciar sesión para canjear el código' });
+    }
+
+    // Crear un nuevo registro en CodeUsage
+    const newCodeUsage = new CodeUsage({
+      codeId: code._id,
+      userId: userId,
+      fechaUso: new Date()
+    });
+    await newCodeUsage.save();
+
+    // Marcar el código como usado
+    code.usado = true;
+    await code.save();
+    
+    res.json({ 
+      message: 'Código válido', 
+      premio: code.premio 
+    });
+
   } catch (error) {
     console.error('Error al verificar código:', error);
     next(error);
